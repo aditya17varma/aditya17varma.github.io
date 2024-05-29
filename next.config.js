f2358f3b4468b98779a218b64108ee8fa4c003d5
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2024-05-28 20:37:49 -0700

    server actions allowed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-
-    basePath: "/aditya17varma.github.io",
-
-    images: {
-        unoptimized: true,
-    },
 };
 
 module.exports = nextConfig
